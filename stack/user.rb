0fdd8242d2adf0dcd809933b225f209e788d354e
Garry Hill <garry@magnetised.info>
2013-08-19 09:42:09 +0100

    Accounts come with an implicit trust of github
    i.e. add GH's public key to the known_hosts file

--- stack/user.rb
+++ stack/user.rb
@@ -2,6 +2,7 @@ package :user do
   requires :create_shared_group, opts
   requires :create_user, opts
   requires :add_keys, opts
+  requires :known_hosts, opts
   requires :default_environment, opts
 end
 
@@ -28,12 +29,20 @@ package :add_keys do
   authorized_keys = "#{ssh_dir}/authorized_keys"
   pub = %w(id_rsa.pub id_dsa.pub).map { |key| File.join(ENV['HOME'], '.ssh', key) }.detect { |file| File.exist?(file) }
   runner "test -d #{ssh_dir} || sudo -u #{opts[:user]} mkdir -p #{ssh_dir}"
-  file authorized_keys, contents: File.read(pub), mode: "0600"
-  runner "test -f #{authorized_keys} && chown #{opts[:user]}:#{opts[:user]} #{authorized_keys}"
+  file authorized_keys, contents: File.read(pub), mode: "0600", owner: ([opts[:user]] * 2).join(':')
   verify do
     has_file authorized_keys
   end
 end
+
+package :known_hosts do
+  known_hosts = "/home/#{opts[:user]}/.ssh/known_hosts"
+  file known_hosts, contents: File.read(File.expand_path('../../templates/home/known_hosts', __FILE__)), owner: ([opts[:user]] * 2).join(':'), mode: "0600"
+  verify do
+    has_file known_hosts
+  end
+end
+
 
 package :default_environment do
   file "/etc/profile.d/spontaneous_env.sh", contents: "export SPOT_ENV=production\n", mode: "0644"

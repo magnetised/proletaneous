79d389188ed54108d0feeb12ca7b6e0a678311d7
Garry Hill <garry@magnetised.info>
2013-08-28 12:12:24 +0100

    Include cron task to delete old backups

--- stack/postgresql.rb
+++ stack/postgresql.rb
@@ -72,16 +72,31 @@ end
 # make base backup every day @ 2am
 package :wal_e_crontab do
   tmpfile = "/tmp/wal-e.cron"
-  # need to delete older backups. something like the following will get the list of backup names
+  # need to delete older backups.
+  #  The following will get the backup id of the backup that happened 7 days ago
+  #  The parameter to `tail` determines how many days of backups to keep
+  #  assuming that you do one base backup per day
   #
-  # $(/usr/bin/chpst -e /etc/wal-e.d/env /usr/local/bin/wal-e backup-list | sed '1d' | awk '{print $1}')
+  # $(/usr/bin/chpst -e /etc/wal-e.d/env /usr/local/bin/wal-e backup-list | tail -7 | head -1 | awk '{print $1}')
+  #
+  # `backup-list` lists the backups in increasing date order
+  #
+  #  the sed command deletes the first line which is the header line -- this is important because if the number of backups is less than the number of days
+  #  we still want to return a valid backup id, rather than the header
   #
   # then I need to run 'delete before':
   #
   # /usr/bin/chpst -e /etc/wal-e.d/env /usr/local/bin/wal-e delete before base_00000004000002DF000000A6_03626144
+
+  backup_days_to_keep = opts[:db_backup_days] || 7
+  wal_e = "/usr/bin/chpst -e /etc/wal-e.d/env /usr/local/bin/wal-e"
   crontab = [
-     "0 2 * * * /usr/bin/chpst -e /etc/wal-e.d/env /usr/local/bin/wal-e backup-push /var/lib/postgresql/9.1/main"
+     "# Make a base backup of the db every night at midnight",
+     "0 0 * * * #{wal_e} backup-push /var/lib/postgresql/9.1/main",
+     "# Delete old backups every night at 3am, keeping #{backup_days_to_keep} days",
+     "0 3 * * * #{wal_e} delete --confirm before $(#{wal_e} backup-list | sed '1d' | tail -#{backup_days_to_keep} | head -1 | awk '{print $1}' )"
   ].join("\n")
+  puts crontab
   file tmpfile, contents: crontab << "\n"
   runner "crontab -u postgres #{tmpfile} ; rm #{tmpfile}"
 end
